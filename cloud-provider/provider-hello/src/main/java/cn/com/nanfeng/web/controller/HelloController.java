b0a41732bb7761f9daec718b54d366fdced59d67
liutao <liu05265218@162.com>
2019-11-29 09:42:59 +0800

    【添加】配置多数据源

--- cloud-provider/provider-hello/src/main/java/cn/com/nanfeng/web/controller/HelloController.java
+++ cloud-provider/provider-hello/src/main/java/cn/com/nanfeng/web/controller/HelloController.java
@@ -3,11 +3,14 @@ package cn.com.nanfeng.web.controller;
 import cn.com.nanfeng.exception.BusinessException;
 import cn.com.nanfeng.service.IHelloService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 /**
@@ -17,6 +20,7 @@ import java.time.LocalDate;
  * @date 2019-09-01 14:24
  */
 @RestController
+@Validated
 public class HelloController {
 
     @Resource
@@ -34,5 +38,11 @@ public class HelloController {
     @GetMapping("/error")
     public void errorTest(){
         helloService.testError();
+    }
+
+    @GetMapping("/test1")
+    public String test(@NotBlank(message = "{required}")String name,
+                       @Email(message = "{invalid}")String email){
+        return "success";
     }
 }
